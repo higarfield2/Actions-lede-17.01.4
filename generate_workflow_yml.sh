35d8909cbddb27c19ecd2129319b2a96d4b7a602
HiGarfield <HiGarfield@126.com>
2021-02-26 02:54:36 +0800

    first commit

---
+++ generate_workflow_yml.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+generate_device_yml() {
+	local device_name="$1"
+	sed "s/template_device_please_replace/${device_name}/" \
+		".github/build-lede-template.yml" \
+		>".github/workflows/build-lede-${device_name}.yml"
+}
+
+mkdir -p ".github/workflows/"
+rm -rf ".github/workflows/*"
+
+for file in conf/.config.*; do
+	dev_name="$(basename "$file" | sed 's/^\.config\.//')"
+	generate_device_yml "$dev_name"
+done
+
+cp ".github/lede-dl.yml" ".github/workflows/lede-dl.yml"
+cp ".github/update-checker.yml" ".github/workflows/update-checker.yml"
